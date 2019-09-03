c396cc0b8c9b3293a00ac8a89777bf4fd1127519
JonLoss55812 <48960451+JonLoss55812@users.noreply.github.com>
2019-09-03 13:04:12 -0500

    next step

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from numpy import *
 
 def run(): 
-  points = genfromtext
-
-
-
+  points = genfromtext('stockdata.csv', delimiter=',')
+  #hyperparameter
+  learning_rate = 0.0001
+  #y = mx+b
+  inital_b = 0
+  initial_m = 0
+  num_iteration = 1000
+  #feed into model runner, seymor
+  [b,m] = gradient_decent_runner(points,inital_b,initial_m,num_iteration)
+  print(b)
+  print(m)
+  
+  
 if__name__= '__main__';
   run()
